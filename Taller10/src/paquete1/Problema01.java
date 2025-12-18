6ccfdccd488d41058fe3b190de52b6b9bbcbd124
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-12-18 11:00:42 -0500

    Problema 01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,45 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+        int[]diasAsistidos = new int[6];
+        String reporte = "";
+        String condicion = "";
+        
+        for (int i = 0; i < asistencia.length; i++) { 
+            diasAsistidos[i]=0;
+            for (int j = 0; j < asistencia[i].length; j++) { 
+                if (asistencia[i][j]){
+                    diasAsistidos[i]++;
+                }
+                     
+            }
+            
+        }
+        
+        for (int i = 0; i < estudiantes.length; i++) {
+            if(diasAsistidos[i]==5){
+                condicion = "Su asistencia es completa";
+            }else{
+                condicion = "su asistencia es incompleta o no valida";
+                
+            }
+            reporte = String.format("%sEl/La estudiante %s asistio %s dias\n%s\n ",
+                    reporte,
+                    estudiantes[i],
+                    diasAsistidos[i],
+                    condicion);
+        }
+        
+        System.out.printf("%s\n", reporte);
     }
-    
+
 }
