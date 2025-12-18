1a3ad101e54dfe60c1ca6a7d9924dcb21c983bc5
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2025-12-18 12:17:09 -0500

    Problema 05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,31 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {{"Sarah Ward", "Philip Payne"},
+        {"Carrie Burton", "Lauren Rice"},
+        {"Emma Escobar", "Lori Flores"},
+        {"Steven West", "Toni Martin"}};
+
+        String acumulador = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+
+                String nombre = estudiantes[i][j];
+                char letra = Character.toLowerCase(nombre.charAt(0));
+
+                switch (letra) {
+                    case 's':
+                    case 'p':
+                    case 't':
+                        acumulador = acumulador + nombre + "\n";
+                        break;
+                }
+            }
+
+        }
+        System.out.printf("Los estudiantes con inicial S, P o T son: \n%s\n", acumulador);
+    }
 }
